67057e014348fb8de1ec885e3c2c368e66b5c4e1
iliyan <maximizebg@gmail.com>
2022-11-20 11:43:09 +0000

    Bevy updated to 0.9

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use bevy::prelude::*;
 fn main() {
   App::new()
     .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
-    .insert_resource(WindowDescriptor {
-      title: "Game-1".to_string(),
-      width: 598.0,
-      height: 776.0,
-      ..Default::default()
-    })
     .add_plugins(DefaultPlugins)
     .run();
 }
+
+// WindowDescriptor {
+//   title: "Game-1".to_string(),
+//   width: 598.0,
+//   height: 776.0,
+//   ..Default::default()
+// }
